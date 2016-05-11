f79e8baabc5745f01aa1818b75fd99bbb9e5e3ce
Jean-baptiste GUTHERTZ <jguthert@e2r9p4.42.fr>
2016-05-11 19:16:39 +0200

    git ignore updated + mod list added

---
+++ srcs/mod_list.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mod_list.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/11 17:54:18 by jguthert          #+#    #+#             */
+/*   Updated: 2016/05/11 18:31:37 by jguthert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+
+void		make_list(t_ftl_root *root, char **av, int ac)
+{
+	t_elem	elem;
+
+	while (--ac > 0)
+	{
+		elem.name = av[ac];
+		elem.selected = 0;
+		ftl_push_front(root, (FTL_NODE *)(&elem));
+	}
+}
